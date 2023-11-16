6023eb95e48cff2fe81c33d769bc798000d3c581
Alp SARICA <alp.sarica@eteration.com>
2023-11-16 14:29:52 +0300

    initial commit

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
-#
-#  Copyright (c) 2022 Composiv.ai, Eteration A.S. and others
-#
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v2.0
-# and Eclipse Distribution License v1.0 which accompany this distribution.
-#
-# The Eclipse Public License is available at
-#    http://www.eclipse.org/legal/epl-v10.html
-# and the Eclipse Distribution License is available at
-#   http://www.eclipse.org/org/documents/edl-v10.php.
-#
-# Contributors:
-#    Composiv.ai, Eteration A.S. - initial API and implementation
-#
-#
-#!/usr/bin/env python
-
 from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+import os
+from glob import glob
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['agent','agent.mqtt','agent.ros', 'agent.ros.converter'],
-    package_dir={'': 'src'})
+package_name = 'agent'
 
-setup(**setup_args)
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[
+        package_name,
+        f"{package_name}.ros",
+        f"{package_name}.ros.msg_converter"
+    ],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Alp SARICA',
+    maintainer_email='alp.sarica@eteration.com',
+    description='Eclipse Muto Agent Package',
+    license='Eclipse Public License v2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'muto_agent = agent.muto_agent:main',
+            'mqtt = agent.mqtt:main',
+            'commands = agent.commands:main'            
+        ],
+    },
+)
